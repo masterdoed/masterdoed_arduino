8d6a92252d4fca6b4fe5860597ecfc6b8157dad5
masterdoed <github@doedspace.de>
2016-02-18 15:23:26 +0000

    ...

--- readSerial.py
+++ readSerial.py
@@ -3,9 +3,14 @@ import serial
 import datetime
 import time
 
+myFile = open('weatherReadings.txt', 'w')
+
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 
 while True:
  line = ser.readline()
  now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
- print now+","+line
+ #print now+","+line
+ myFile.write (now+","+line)
+
+myFile.close()
